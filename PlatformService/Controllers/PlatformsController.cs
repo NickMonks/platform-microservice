7f59b8b3eb69cb1418fca4929f3501f1e582cfd2
NickMonks <nicolasmg94@gmail.com>
2021-12-08 19:40:58 +0000

    Added dockerfile

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,66 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers 
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms ...");
+            var platformItem = _repository.GetAllPlatforms();
+
+            // we map from our collection of models to an ienumerable of read dto's
+            // by using the first createmap profile. 
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name="GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            
+            // return 404 response
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            // map to a platform object to create platform in the database from the repository
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            // return the object with the status 200 OK code if successful
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            // creates a 201 status code (created), and it will return the 'location' header (which defines)
+            // the URL of the resource created using REST. 
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id}, platformReadDto);
+        }
+
+
+    }
+}
